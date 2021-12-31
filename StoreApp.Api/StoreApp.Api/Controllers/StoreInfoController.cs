e890e5a60d03a28cebee93093f1ad864a3881b67
Jingjing Zhuang <jzhuang3219@gmail.com>
2021-12-30 21:13:55 -0500

    Added a simple logger to StoreInfoController.

--- StoreApp.Api/StoreApp.Api/Controllers/StoreInfoController.cs
+++ StoreApp.Api/StoreApp.Api/Controllers/StoreInfoController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using StoreApi.DataStorage;
 using StoreApi.Logic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StoreApp.Api.Controllers
 {
@@ -10,25 +11,39 @@ namespace StoreApp.Api.Controllers
     public class StoreInfoController : ControllerBase
     {
         private readonly IRepository _repository;
-        public StoreInfoController(IRepository repository)
+        private readonly ILogger<StoreInfoController> _logger;
+        public StoreInfoController(IRepository repository, ILogger<StoreInfoController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
-        // GET api/stores
+        // GET api/storeinfo
         [HttpGet]
         public async Task<IEnumerable<Location>> GetStoreLocations()
         {
-            IEnumerable<Location> stores = await _repository.GetLocationListAsync();
-
+            IEnumerable<Location> stores;
+            stores = await _repository.GetLocationListAsync();
+            _logger.LogInformation("*** GET location list ***");
             return stores.ToList();
         }
 
-        // GET api/store/{id}
-        [HttpGet("id")]
-        public async Task<IEnumerable<Product>> GetStoreProducts(string id)
+        // ActionResult types represent various HTTP status codes.
+        // GET api/storeinfo/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetStoreProducts([Required] string id)
         {
-            IEnumerable<Product> products = await _repository.GetStoreProductsAsync(id);
+            IEnumerable<Product> products;
+            try
+            {
+                _logger.LogInformation("*** GET products from store id {id} ***", id);
+                products = await _repository.GetStoreProductsAsync(id);
+            }
+            catch (SqlException e)
+            {
+                _logger.LogError(e, "*** SQL ERROR! Unable to GET store products... ***");
+                return StatusCode(500);
+            }
             return products.ToList();
         }
     }
