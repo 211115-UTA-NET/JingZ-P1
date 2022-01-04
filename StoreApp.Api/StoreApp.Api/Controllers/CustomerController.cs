06a6685704f4cf3eccf21525db13c36d8444b374
Jingjing Zhuang <jzhuang3219@gmail.com>
2022-01-04 00:01:34 -0500

    Able to GET/POST customer information using api.

---
+++ StoreApp.Api/StoreApp.Api/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using StoreApi.DataStorage;
+using StoreApi.Logic;
+using StoreApp.Api.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IRepository _repository;
+        private readonly ILogger<CustomerController> _logger;
+        public CustomerController(IRepository repository, ILogger<CustomerController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        // POST api/customer
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateAccount(string firstName, string lastName)
+        {
+            int id = -1;
+            try
+            {
+                _logger.LogInformation("*** [POST] Add customer: {firstName} {lastName} ***", firstName, lastName);
+                id = await _repository.AddNewCustomerAsync(firstName, lastName);
+            } catch (SqlException ex)
+            {
+                _logger.LogError(ex, "*** SQL ERROR! Unable to [POST] Add customer... ***");
+                return StatusCode(500);
+            }
+            return id;
+        }
+
+        // GET api/customer?customerid=forgot&firstname=...&lastname=...
+        // GET api/customer?customerid=...&firstname=""&lastname=""
+        [HttpGet("login")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetAccount([FromQuery, Required] CustomerLogin customer)
+        {
+            IEnumerable<Customer> customerInfo;
+            try
+            {
+                _logger.LogInformation("*** [GET] customer ID# {id}: {firstName} {lastName} ***", customer.CustomerID, customer.FirstName, customer.LastName);
+                customerInfo = await _repository.FindCustomerAsync(customer.CustomerID + "", customer.FirstName!, customer.LastName!);
+            }
+            catch (SqlException ex)
+            {
+                _logger.LogError(ex, "*** SQL ERROR! Unable to [Get] customer information... ***");
+                return StatusCode(500);
+            }
+            return customerInfo.ToList();
+        }
+    }
+}
---
+++ StoreApp.Api/StoreApp.Api/Dtos/customerLogin.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreApp.Api.Dtos
+{
+    public class CustomerLogin
+    {
+        /// <summary>
+        ///     store customer ID from 'Customer' db table or user input
+        /// </summary>
+        [Required]
+        public string? CustomerID { get; set; }
+        /// <summary>
+        ///     store customer last name from 'Customer' db table or user input
+        /// </summary>
+        [Required]
+        public string? LastName { get; set; }
+        /// <summary>
+        ///     store customer first name from 'Customer' db table or user input
+        /// </summary>
+        [Required]
+        public string? FirstName { get; set; }
+    }
+}
